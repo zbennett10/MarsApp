2463ab5c472a6c498ec565e0fca0009e124a22a8
zbennett10 <zbennett10@Hotmail.com>
2016-11-15 21:43:13 -0500

    Add project files.

---
+++ AllAboutMars/MainPage.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using LinqToTwitter;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AllAboutMars
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public static ListBox twitterTest = new ListBox();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            twitterStack.Children.Add(twitterTest);
+        }
+
+        private static SingleUserAuthorizer authorizer = new SingleUserAuthorizer
+        {
+            CredentialStore = new SingleUserInMemoryCredentialStore
+            {    
+            }
+        };
+
+        private void OnMainPage_Load(object sender, RoutedEventArgs e)
+        {
+            var resources = new Windows.ApplicationModel.Resources.ResourceLoader("Resources");
+            authorizer.CredentialStore.ConsumerKey = resources.GetString("consumerToken");
+            authorizer.CredentialStore.ConsumerSecret = resources.GetString("consumerSecret");
+            authorizer.CredentialStore.OAuthToken = resources.GetString("accessToken");
+            authorizer.CredentialStore.OAuthTokenSecret = resources.GetString("accessSecret");
+
+            var results = SearchTwitter("Mars Nasa SpaceX");
+            twitterTest.Items.Clear();
+            results.ForEach(tweet => twitterTest.Items.Add(tweet.User.Name + ":" + tweet.Text));
+
+        }
+
+        private static List<Status> SearchTwitter(string searchTerm)
+        {
+            var twitterContext2 = new TwitterContext(authorizer);
+
+            var srch =
+               Enumerable.SingleOrDefault((from search in twitterContext2.Search
+                                           where search.Type == SearchType.Search &&
+                                           search.Query == searchTerm &&
+                                           search.Count == 10
+                                           select search));
+            if (srch != null && srch.Statuses.Count > 0)
+            {
+                return srch.Statuses.ToList();
+            }
+
+            return new List<Status>();
+        }
+
+        //private List<Status> list;
+
+        //private void GetMostRecent10HomeTimeLine()
+        //{
+        //    var twitterContext = new TwitterContext(authorizer);
+
+
+        //    var tweets = from tweet in twitterContext.Status
+        //                 where tweet.Type == StatusType.Home &&
+        //                 tweet.Count == 5
+        //                 select tweet;
+
+        //    list = tweets.ToList();
+        //}
+
+        private async void testNasaButton_Click(object sender, RoutedEventArgs e)
+        {
+            //goes up to at least photos[500]
+            NasaProxy.RootObject data = await NasaProxy.GetNasaData();
+            BitmapImage image = new BitmapImage(new Uri(data.photos[5].img_src));
+
+            testImage.Source = image;
+            //nasaTest.Text = data.photos[0].camera.full_name;
+            nasaTest.Text = data.photos[5].rover.name;
+
+        }
+
+    }
+}
---
+++ AllAboutMars/TwitterProxy.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using LinqToTwitter;
+
+namespace AllAboutMars
+{
+    class TwitterProxy
+    {
+        //private static SingleUserAuthorizer authorizer = new SingleUserAuthorizer {
+        //    CredentialStore = new SingleUserInMemoryCredentialStore
+        //    {
+        //        ConsumerKey = "eS5AVLBVb2w0gwuiG53wat08s",
+        //        ConsumerSecret = "asdkfjmI3UO4xqwKZmWp91RlclieElXZONCUzZDvxZ6DtASNgFOK7Qgo",
+        //        AccessToken = "710985700125048832-RkVkwXqKeWSBKFQQ5bxwTEFeceBAACt",
+        //        AccessTokenSecret = "QXbSK9YPbg5dL3Vd52jOw8rlNLq0ScwJ3prTzvIIcRJJ4"
+        //    }
+
+
+        //};
+
+        
+        //public static List<Status> list;
+        //public static void GetMostRecent10HomeTimeLine()
+        //{
+        //    var twitterContext = new TwitterContext(authorizer);
+            
+
+        //    var tweets = from tweet in twitterContext.Status
+        //                 where tweet.Type == StatusType.Home &&
+        //                 tweet.Count == 5
+        //                 select tweet;
+
+        //    list = tweets.ToList();
+        //}
+
+
+        //public static List<Status> SearchTwitter(string searchTerm)
+        //{
+        //    var twitterContext2 = new TwitterContext(authorizer);
+
+        //    var srch =
+        //       Enumerable.SingleOrDefault((from search in twitterContext2.Search
+        //                                   where search.Type == SearchType.Search &&
+        //                                   search.Query == searchTerm &&
+        //                                   search.Count == 5
+        //                                   select search));
+        //    if (srch != null && srch.Statuses.Count > 0)
+        //    {
+        //        return srch.Statuses.ToList();
+        //    }
+
+        //    return new List<Status>();
+        //}
+
+        
+        
+
+        //private async Task<RootObject> GetTwitterData()
+        //{
+        //    HttpClient http = new HttpClient();
+        //    var response = await http.GetAsync();
+        //    var result = await response.Content.ReadAsStringAsync();
+        //    var serializer = new DataContractJsonSerializer(typeof(RootObject));
+
+
+
+        //}
+
+
+    }
+}
