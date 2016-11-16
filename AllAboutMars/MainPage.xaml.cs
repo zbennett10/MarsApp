a94db7fac24b43dac03cf93174277096ce3be3c9
zbennett10 <zbennett10@Hotmail.com>
2016-11-16 15:21:49 -0500

    Minor Changes

--- AllAboutMars/MainPage.xaml.cs
+++ AllAboutMars/MainPage.xaml.cs
@@ -14,6 +14,8 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 using LinqToTwitter;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -24,12 +26,15 @@ namespace AllAboutMars
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        public static ListBox twitterTest = new ListBox();
+
+        //public static ListBox twitterTest = new ListBox();
+        
+        
 
         public MainPage()
         {
             this.InitializeComponent();
-            twitterStack.Children.Add(twitterTest);
+
         }
 
         private static SingleUserAuthorizer authorizer = new SingleUserAuthorizer
@@ -39,6 +44,7 @@ namespace AllAboutMars
             }
         };
 
+
         private void OnMainPage_Load(object sender, RoutedEventArgs e)
         {
             var resources = new Windows.ApplicationModel.Resources.ResourceLoader("Resources");
@@ -47,21 +53,33 @@ namespace AllAboutMars
             authorizer.CredentialStore.OAuthToken = resources.GetString("accessToken");
             authorizer.CredentialStore.OAuthTokenSecret = resources.GetString("accessSecret");
 
-            var results = SearchTwitter("Mars Nasa SpaceX");
+           
+            var results = SearchTwitter("Mars Nasa");
+            List<Match> links = Link_Finder(results);
             twitterTest.Items.Clear();
             results.ForEach(tweet => twitterTest.Items.Add(tweet.User.Name + ":" + tweet.Text));
-
+            links.ForEach(link => twitterTest.SelectedValue = link);
+          
         }
+
+        private static List<Match> Link_Finder(List<Status> tweetList)
+        {
+           string pattern = @"\b(?:https?:)\S+\b";
+            List<Match> matches = new List<Match>();
+            tweetList.ForEach(tweet => matches.Add(Regex.Match(tweet.Text, pattern)));
+            return matches;     
+       }
 
         private static List<Status> SearchTwitter(string searchTerm)
         {
-            var twitterContext2 = new TwitterContext(authorizer);
+            var twitterContext = new TwitterContext(authorizer);
 
             var srch =
-               Enumerable.SingleOrDefault((from search in twitterContext2.Search
+               Enumerable.SingleOrDefault((from search in twitterContext.Search
                                            where search.Type == SearchType.Search &&
                                            search.Query == searchTerm &&
-                                           search.Count == 10
+                                           search.ResultType ==  ResultType.Popular &&
+                                           search.Count == 5
                                            select search));
             if (srch != null && srch.Statuses.Count > 0)
             {
@@ -69,6 +87,28 @@ namespace AllAboutMars
             }
 
             return new List<Status>();
+        }
+
+        private void twitterTest_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            ListBoxItem lbi = ((sender as ListBox).SelectedItem as ListBoxItem);
+            
+            
+        }
+
+        private void twitterSearchButton_Click(object sender, RoutedEventArgs e)
+        {
+           var searchResult =  SearchTwitter(twitterSearchTextBox.Text);
+            twitterTest.Items.Clear();
+            foreach (var tweet in searchResult)
+            {
+                twitterTest.Items.Add(tweet.User.Name + ":" + tweet.Text);
+            }
+        }
+
+        private void twitterTest_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
 
         //private List<Status> list;
@@ -86,17 +126,17 @@ namespace AllAboutMars
         //    list = tweets.ToList();
         //}
 
-        private async void testNasaButton_Click(object sender, RoutedEventArgs e)
-        {
-            //goes up to at least photos[500]
-            NasaProxy.RootObject data = await NasaProxy.GetNasaData();
-            BitmapImage image = new BitmapImage(new Uri(data.photos[5].img_src));
+        //private async void testNasaButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    //goes up to at least photos[500]
+        //    NasaProxy.RootObject data = await NasaProxy.GetNasaData();
+        //    BitmapImage image = new BitmapImage(new Uri(data.photos[5].img_src));
 
-            testImage.Source = image;
-            //nasaTest.Text = data.photos[0].camera.full_name;
-            nasaTest.Text = data.photos[5].rover.name;
+        //    testImage.Source = image;
+        //    //nasaTest.Text = data.photos[0].camera.full_name;
+        //    //nasaTest.Text = data.photos[5].rover.name;
 
-        }
+        //}
 
     }
 }
