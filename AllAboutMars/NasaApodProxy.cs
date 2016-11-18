e0aefb43e24c9372535f66de64fe1aad95630d0b
zbennett10 <zbennett10@Hotmail.com>
2016-11-18 16:53:56 -0500

    added basic Nasa APOD functionality

---
+++ AllAboutMars/NasaApodProxy.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllAboutMars
+{
+    class NasaApodProxy
+    {
+        public static async Task<RootObject> GetAPOD(string key)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(String.Format("https://api.nasa.gov/planetary/apod?api_key={0}", key));
+            var result = await response.Content.ReadAsStringAsync();
+            var serializer = new DataContractJsonSerializer(typeof(RootObject));
+            var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(result));
+            var data = (RootObject)serializer.ReadObject(memoryStream);
+            return data;
+        }
+
+        [DataContract]
+        public class RootObject
+        {
+            [DataMember]
+            public string copyright { get; set; }
+
+            [DataMember]
+            public string date { get; set; }
+
+            [DataMember]
+            public string explanation { get; set; }
+
+            [DataMember]
+            public string hdurl { get; set; }
+
+            [DataMember]
+            public string media_type { get; set; }
+
+            [DataMember]
+            public string service_version { get; set; }
+
+            [DataMember]
+            public string title { get; set; }
+
+            [DataMember]
+            public string url { get; set; }
+        }
+    }
+}
--- AllAboutMars/NasaPage.xaml.cs
+++ AllAboutMars/NasaPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Resources;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -11,6 +12,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
@@ -22,9 +24,31 @@ namespace AllAboutMars
     /// </summary>
     public sealed partial class NasaPage : Page
     {
+
         public NasaPage()
         {
             this.InitializeComponent();
+        }
+
+        
+
+        private async void On_Page_Load(object sender, RoutedEventArgs e)
+        {
+            NasaApodProxy.RootObject data = await NasaApodProxy.GetAPOD(Nasa_Key_Getter());
+            apodImage.Source = Image_Creator(data.hdurl);
+            descTextBlock.Text = data.explanation;
+        }
+
+        private BitmapImage Image_Creator(string url)
+        {
+            BitmapImage image = new BitmapImage(new Uri(url));
+            return image;
+        }
+
+        private string Nasa_Key_Getter()
+        {
+            ResourceLoader resource = new ResourceLoader("Resources");
+            return resource.GetString("nasaToken");  
         }
 
         private void homeButton_Click(object sender, RoutedEventArgs e)
