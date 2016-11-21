0583ef86ce4a829beab5c521d8db988eecd22259
zbennett10 <zbennett10@Hotmail.com>
2016-11-21 14:01:50 -0500

    Map now displays names of ground stations

--- AllAboutMars/NasaStationPage.xaml.cs
+++ AllAboutMars/NasaStationPage.xaml.cs
@@ -39,15 +39,31 @@ namespace AllAboutMars
             
         }
 
-        
+
 
         private void On_Page_Load(object sender, RoutedEventArgs e)
         {
             stationMap.MapServiceToken = Map_Key_Getter();
-            Pin_Populator(Point_Creator(Position_Creator(Station_Fetcher())));
-            
+            stationNames = new List<string>();
+            Station_Name_Fetcher();
+            Pin_Populator(Point_Creator(Position_Creator(Station_Location_Fetcher())));
+            List<MapIcon> iconList = new List<MapIcon>();
+
+            for (int i = 0; i < stationMap.MapElements.Count; i++)
+            {
+                if (stationMap.MapElements[i] is MapIcon)
+                {
+                    iconList.Add((MapIcon)stationMap.MapElements[i]);
+                }
+            }
+            for (int i = 0; i < iconList.Count; i++)
+            {
+                iconList[i].Title = stationNames[i];
+            }
            
         }
+
+
 
         public List<BasicGeoposition> Position_Creator(List<Location> list)
         {
@@ -69,12 +85,19 @@ namespace AllAboutMars
             return pointList;
         }
 
+        
+
         public void Pin_Populator(List<Geopoint> points)
-        {
+        {   
             foreach(Geopoint point in points)
             {
                 stationMap.MapElements.Add(new MapIcon { Location = point});
+                
             }
+
+
+
+            
         }
 
 
@@ -88,9 +111,9 @@ namespace AllAboutMars
         //[XmlRoot(ElementName = "Location", Namespace = "http://sscweb.gsfc.nasa.gov/schema")]
         public class Location
         {
-            //[XmlElement(ElementName = "Latitude", Namespace = "http://sscweb.gsfc.nasa.gov/schema")]
+            //    //[XmlElement(ElementName = "Latitude", Namespace = "http://sscweb.gsfc.nasa.gov/schema")]
             public string Latitude { get; set; }
-            //[XmlElement(ElementName = "Longitude", Namespace = "http://sscweb.gsfc.nasa.gov/schema")]
+            //    //[XmlElement(ElementName = "Longitude", Namespace = "http://sscweb.gsfc.nasa.gov/schema")]
             public string Longitude { get; set; }
         }
 
@@ -102,28 +125,62 @@ namespace AllAboutMars
             //[XmlElement(ElementName = "Name", Namespace = "http://sscweb.gsfc.nasa.gov/schema")]
             public string Name { get; set; }
             //[XmlElement(ElementName = "Location", Namespace = "http://sscweb.gsfc.nasa.gov/schema")]
-            public Location Location { get; set; }
+            //public Location Location { get; set; }
+            
         }
 
         
-
-    
-        public List<Location> Station_Fetcher()
+        public List<Location> Station_Location_Fetcher()
         {
-            
             XDocument loadedData = XDocument.Load("GroundStationInfo.xml");
             var ns = loadedData.Root.GetDefaultNamespace();
-            
-            var locationData = from location in loadedData.Descendants(ns +"Location")
-                               select new Location
+
+            var locationData = from location in loadedData.Descendants(ns + "Location")
+                              select new Location
+                              {
+                                  
+                                  Latitude = location.Element(ns + "Latitude").Value,
+                                  Longitude = location.Element(ns + "Longitude").Value
+                              };
+            return locationData.ToList();
+        }
+
+        public static List<string> stationNames;
+    
+        public void Station_Name_Fetcher()
+        {
+
+            XDocument loadedData = XDocument.Load("GroundStationInfo.xml");
+            var ns = loadedData.Root.GetDefaultNamespace();
+
+            var nameData = from station in loadedData.Descendants(ns + "GroundStation")
+                               select new GroundStation
                                {
-                                   Latitude = location.Element(ns + "Latitude").Value,
-                                   Longitude = location.Element(ns + "Longitude").Value
+                                   Name = station.Element(ns + "Name").Value
                                };
 
-            return locationData.ToList();
-                
+            foreach(GroundStation station in nameData)
+            {
+                stationNames.Add(station.Name);
+            }
+           
         }
+
+        //private void Station_Name_Fetcher()
+        //{
+        //    XDocument loadedData = XDocument.Load("GroundStationInfo.xml");
+        //    var ns = loadedData.Root.GetDefaultNamespace();
+        //    var stationData = from station in loadedData.Descendants(ns + "GroundStation")
+        //                   select new GroundStation
+        //                   {
+        //                       Name = station.Element("Name").Value
+        //                   };
+        //    List<GroundStation> list = stationData.ToList();
+        //    foreach(GroundStation station in list)
+        //    {
+        //        stationList.Add(station);
+        //    }
+        //}
 
         private void homeButton_Click(object sender, RoutedEventArgs e)
         {
