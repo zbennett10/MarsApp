584f09cb85b93d4aedc9280b966a0ac5995d45e1
zbennett10 <zbennett10@Hotmail.com>
2016-11-25 10:29:42 -0500

    Did a lot of refactoring on the main page and changed the tweet feed to show only tweets from NASA and SpaceX.

--- AllAboutMars/MainPage.xaml.cs
+++ AllAboutMars/MainPage.xaml.cs
@@ -16,6 +16,7 @@ using Windows.UI.Xaml.Navigation;
 using LinqToTwitter;
 using System.Text.RegularExpressions;
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -28,11 +29,7 @@ namespace AllAboutMars
     {
         //TODO
         //try to implement xmlreader instead of xdocument
-        //change search to get a set amount of recent tweets from differ user tweet history
-        //-NASA, SpaceX, National Geographic, Space.com
-        //edit display to show a scrollviewer/listbox for each username
         //display username above scrollviwer and edit out http part of tweet so that only message shows
-
 
         public MainPage()
         {
@@ -46,35 +43,26 @@ namespace AllAboutMars
             }
         };
 
-        public List<Match> twitterLinks = new List<Match>();
+        public List<Match> spaceXLinks = new List<Match>();
+        public List<Match> nasaLinks = new List<Match>();
 
         private void On_Page_Load(object sender, RoutedEventArgs e)
         {
             Authorizer_Key_Populator();        
-            Link_List_Populator(Mars_Link_Fetcher(), Nasa_Link_Fetcher());
-            twitterTest.Items.Clear();
-            Search_Result_Mars_Fetcher().ForEach(tweet => twitterTest.Items.Add(tweet.User.Name + ":" + tweet.Text));
-            Search_Result_Nasa_Fetcher().ForEach(tweet => twitterTest.Items.Add(tweet.User.Name + ":" + tweet.Text)); 
+            Link_List_Populator(SpaceX_Link_Fetcher(), Nasa_Link_Fetcher());
+            spaceXLB.Items.Clear();
+            Timeline_Fetcher("SpaceX").ForEach(tweet => spaceXLB.Items.Add(tweet.User.Name + ":" + tweet.Text));
+            Timeline_Fetcher("Nasa").ForEach(tweet => nasaLB.Items.Add(tweet.User.Name + ":" + tweet.Text)); 
         }
 
-       private List<Status> Search_Result_Mars_Fetcher()
+        private List<Match> SpaceX_Link_Fetcher()
         {
-            return SearchTwitter("Mars SpaceX");
-        }
-
-        private List<Status> Search_Result_Nasa_Fetcher()
-        {
-            return SearchTwitter("Nasa Mars");
-        }
-
-        private List<Match> Mars_Link_Fetcher()
-        {
-            return Link_Finder(Search_Result_Mars_Fetcher());
+            return Link_Finder(Timeline_Fetcher("SpaceX"));
         }
 
         private List<Match> Nasa_Link_Fetcher()
         {
-            return Link_Finder(Search_Result_Nasa_Fetcher());
+            return Link_Finder(Timeline_Fetcher("Nasa"));
         }
         
        private void Authorizer_Key_Populator()
@@ -94,39 +82,38 @@ namespace AllAboutMars
             return matches;     
        }
 
-        private void Link_List_Populator(List<Match> list1, List<Match> list2)
+        private void Link_List_Populator(List<Match> spaceXMatches, List<Match> nasaMatches)
         {
-            list1.ForEach(link => twitterLinks.Add(link));
-            list2.ForEach(link => twitterLinks.Add(link));
+            spaceXMatches.ForEach(link => spaceXLinks.Add(link));
+            nasaMatches.ForEach(link => nasaLinks.Add(link));
         }
 
-        private static List<Status> SearchTwitter(string searchTerm)
-        {
-            var twitterContext = new TwitterContext(authorizer);
 
-            var srch =
-               Enumerable.SingleOrDefault((from search in twitterContext.Search
-                                           where search.Type == SearchType.Search &&
-                                           search.Query == searchTerm &&
-                                           search.ResultType ==  ResultType.Popular &&
-                                           search.Count == 7
-                                           select search));
-            if (srch != null && srch.Statuses.Count > 0)
+        private  static List<Status> Timeline_Fetcher(string user)
+        {
+            using (var twitterContext = new TwitterContext(authorizer))
             {
-                return srch.Statuses.ToList();
+                var tweets = (from tweet in twitterContext.Status
+                              where tweet.Type == StatusType.User
+                              && tweet.ScreenName == user
+                              && tweet.Count == 10                            
+                              select tweet).ToList();
+                return tweets;
             }
-            return new List<Status>();
+           
         }
 
-        private async void twitterTest_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            int selectedIndex = twitterTest.SelectedIndex;
-            string uriToLaunch = twitterLinks[selectedIndex].ToString();
-            var uri = new Uri(uriToLaunch);
+        private async void spaceXLB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {        
+            var uri = new Uri(spaceXLinks[spaceXLB.SelectedIndex].ToString());
             await  Windows.System.Launcher.LaunchUriAsync(uri);
         }
 
-        
+        private async void nasaLB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var uri = new Uri(nasaLinks[nasaLB.SelectedIndex].ToString());
+            await Windows.System.Launcher.LaunchUriAsync(uri);
+        }
 
         private void homeButton_Click(object sender, RoutedEventArgs e)
         {
